396a5ad1ef86f0587939951ece1cb887d9f30480
Jainik Patel <jainikhpatel@yahoo.com>
2018-07-20 12:42:57 +0530

    -Update product , delete product controller -Error scenario for find a product -Homepage text, title change

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -24,20 +24,80 @@ exports.all_product_details = function (req, res) {
 
 exports.product_details = function (req, res) {
     Product.ProductSchema.findById(req.params.id).then(product => {
+      if (!product) {
+        return res.render("error", {
+          message: "Page not found.",
+          error: {
+            status: 404,
+          }
+        });
+      }
       res.send(product);
     });
 };
 
 exports.product_update = function (req, res) {
-    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
-        if (err) return next(err);
-        res.send('Product udpated.');
+    Product.ProductSchema.update({
+      name: req.body.name,
+      price: req.body.price
+    }, {
+      where: {
+        id: req.params.id
+      }
     });
+
+// with error handling
+    /*Product.ProductSchema.findOne({
+        where: {
+          id: req.params.id
+        }
+      }).then(product => {
+        if (!product) {
+          return res.render("error", {
+            message: "Page not found.",
+            error: {
+              status: 404,
+            }
+          });
+        }
+
+        product.update({
+          name: req.body.name,
+          price: req.body.price
+        });
+    });*/
+
+    res.send('Product Updated successfully')
 };
 
 exports.product_delete = function (req, res) {
-    Product.findByIdAndRemove(req.params.id, function (err) {
+    /*Product.findByIdAndRemove(req.params.id, function (err) {
         if (err) return next(err);
         res.send('Deleted successfully!');
-    })
+    })*/
+
+    /*Product.ProductSchema.destroy({
+      where: {
+        id: req.params.id
+      }
+    });*/
+
+    Product.ProductSchema.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(product => {
+      if (!product) {
+        return res.render("error", {
+          message: "Page not found.",
+          error: {
+            status: 404,
+          }
+        });
+      }
+
+      product.destroy();
+    });
+    res.send('Product deleted successfully')
+
 };
